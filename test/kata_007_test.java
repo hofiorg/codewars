90da0dbe7bfcefb95902afe81376383c1a563683
hofiorg <hofiorg@googlemail.com>
2017-12-16 00:52:18 +0100

    kata_007 - refactoring

--- test/kata_007_test.java
+++ test/kata_007_test.java
@@ -1,13 +1,11 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-
 public class kata_007_test {
-    @Test
-    public void test() {
-        System.out.println("printerError Fixed Tests");
-        String s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
-        assertEquals("3/56", kata_007.printerError(s));
-    }
+  @Test
+  public void test() {
+    System.out.println("printerError Fixed Tests");
+    String s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+    assertEquals("3/56", kata_007.printerError(s));
+  }
 }
